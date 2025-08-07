c94517244c436a5ba1e860ee734de8b3319bdd0d
Shruti-lab <124026034+Shruti-lab@users.noreply.github.com>
2025-08-07 13:23:17 +0530

    created project and added files

---
+++ transaction-monitoring-backend-API/src/main/java/com/example/transactionmonitoringbackendapi/TransactionMonitoringBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionmonitoringbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionMonitoringBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionMonitoringBackendApiApplication.class, args);
+	}
+
+}
---
+++ transaction-monitoring-backend-API/src/test/java/com/example/transactionmonitoringbackendapi/TransactionMonitoringBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactionmonitoringbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionMonitoringBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
